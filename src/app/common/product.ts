c13bd1da7f2a8cf474afcd84e4bf41bf25403090
bhuvaneshvar <bhuvaneshvar6@outlook.com>
2021-03-07 17:03:10 +0530

    v1

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  sku: string;
+  name: string;
+  description: string;
+  unitPrice: string;
+  imgUrl: string;
+  active: boolean;
+  unitsInStock: number;
+  dateCreated: Date;
+  lastUsed: Date;
+}
